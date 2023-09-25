c4c995280e7172fbb116bd46cbc8b5d1d5eb71c1
nielswd23 <nielsd@uci.edu>
2023-09-25 16:07:38 -0700

    update shell script and add R file

--- token_noise.sh
+++ token_noise.sh
@@ -2,3 +2,9 @@
 python /Users/niels/Desktop/FG_project/noise_project/noisy_input_tokens.py $1 $2 $3
 
 # now leaning a FG on created stimuli
+cd ~/Desktop/FG_project/noise_project/fg-source-code-restore/
+python /Users/niels/Desktop/FG_project/noise_project/fg_learn_command.py $2 $3
+
+# R grammar fix to format the grammar output
+cd ..
+Rscript grammar_fix.R $2 $3
